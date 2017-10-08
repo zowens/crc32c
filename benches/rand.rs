f2a227bb75d8879ae0333a2a6e7933165044d2b3
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-10-08 13:06:11 +0300

    Rewrite CRC32 algorithm in Rust

---
+++ benches/rand.rs
@@ -0,0 +1,21 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+
+extern crate rand;
+use rand::{OsRng, Rng};
+
+extern crate crc32c;
+use crc32c::crc32c;
+
+
+#[bench]
+fn crc(b: &mut Bencher) {
+    let mut bytes = [0u8; 8192];
+
+    let mut r = OsRng::new().unwrap();
+    r.fill_bytes(&mut bytes);
+
+    b.iter(|| crc32c(&bytes));
+}
