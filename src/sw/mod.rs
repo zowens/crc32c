2d67e3e864f6d72cd5f4b61e3dfcee4a408daf3f
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-10-08 19:03:05 +0300

    Improve performance of software algorithm

--- src/sw/mod.rs
+++ src/sw/mod.rs
@@ -3,39 +3,41 @@
 use util;
 
 mod table;
-use self::table::crc_at;
+use self::table::CrcTable;
 
 /// Software implementation of the algorithm.
 pub fn crc32c(crci: u32, buffer: &[u8]) -> u32 {
-    let mut crc = !crci as u64;
+    let table = CrcTable::table();
+
+    let mut crc = (!crci) as u64;
 
     let (start, mid, end) = util::split(buffer);
 
-    crc = crc_unaligned(crc, start);
+    crc = crc_u8(table, crc, start);
 
-    crc = crc_aligned(crc, mid);
+    crc = crc_u64(table, crc, mid);
 
-    crc = crc_unaligned(crc, end);
+    crc = crc_u8(table, crc, end);
 
     !(crc as u32)
 }
 
-fn crc_unaligned(crci: u64, buffer: &[u8]) -> u64 {
-    buffer.iter().fold(crci, |crc, &next| {
+fn crc_u8(table: &CrcTable, crc: u64, buffer: &[u8]) -> u64 {
+    buffer.iter().fold(crc, |crc, &next| {
         let index = (crc ^ (next as u64)) as u8;
-        crc_at(0, index) ^ (crc >> 8)
+        table.at(0, index) ^ (crc >> 8)
     })
 }
 
-fn crc_aligned(crci: u64, buffer: &[u64]) -> u64 {
+fn crc_u64(table: &CrcTable, crci: u64, buffer: &[u64]) -> u64 {
     buffer.iter().fold(crci, |crc, &next| {
         let crc = crc ^ next;
 
-        (1..8).fold(crc_at(7, crc as u8), |tmp, i| {
+        (1..8).fold(table.at(7, crc as u8), |tmp, i| {
             let row = 7 - i;
             let shift = 8 * i;
             let column = (crc >> shift) as u8;
-            tmp ^ crc_at(row, column)
+            tmp ^ table.at(row, column)
         })
     })
 }
