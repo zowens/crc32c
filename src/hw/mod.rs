85aec43966db0fb51f0e0bd81d6cf9554e921159
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-10-09 17:09:19 +0300

    Fix parallel code

--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -6,10 +6,7 @@ use stdsimd::vendor as simd;
 
 mod table;
 
-use std::{mem, slice};
-
 /// Computes CRC-32C using the SSE 4.2 hardware instruction.
-#[target_feature = "+sse4.2"]
 pub fn crc32c(crci: u32, buffer: &[u8]) -> u32 {
     let mut crc0 = (!crci) as u64;
 
@@ -22,68 +19,24 @@ pub fn crc32c(crci: u32, buffer: &[u8]) -> u32 {
     // meaning we must use 3 of them at a time, to leverage
     // hardware parallelism.
 
-    let mut len = middle.len();
-    let mut ptr: *const u64 = unsafe { mem::transmute(middle.as_ptr()) };
+    let chunk_size = (table::LONG * 3) / 8;
+    let last_chunk = middle.len() / chunk_size * chunk_size;
 
-    unsafe {
-        let chunk_length = table::LONG;
+    let (middle_first, middle_last) = middle.split_at(last_chunk);
 
-        while len >= table::LONG * 3 {
-            let mut crc1 = 0;
-            let mut crc2 = 0;
+    let crc_good = crc_u64(crc0, middle_first);
+    crc0 = crc_u64_parallel3(crc0, chunk_size, &table::LONG_TABLE, middle_first);
 
-            let slice0 = slice::from_raw_parts(ptr, chunk_length);
-            let slice1 =
-                slice::from_raw_parts(ptr.offset((table::LONG / 8) as isize), chunk_length);
-            let slice2 =
-                slice::from_raw_parts(ptr.offset(((table::LONG / 8) * 2) as isize), chunk_length);
+    assert_eq!(crc_good, crc0);
 
-            for i in 0..chunk_length / 8 {
-                crc0 = self::simd::_mm_crc32_u64(crc0, slice0[i]);
-                crc1 = self::simd::_mm_crc32_u64(crc1, slice1[i]);
-                crc2 = self::simd::_mm_crc32_u64(crc2, slice2[i]);
-            }
+    let chunk_size = (table::SHORT * 3) / 8;
+    let last_chunk = middle_last.len() / chunk_size * chunk_size;
 
-            crc0 = table::LONG_TABLE.shift(crc0) ^ crc1;
-            crc0 = table::LONG_TABLE.shift(crc0) ^ crc2;
+    let (middle_last_first, middle_last_last) = middle_last.split_at(last_chunk);
 
-            len -= table::LONG * 3;
-            ptr = ptr.offset(((table::LONG / 8) * 3) as isize);
-        }
-    }
+    crc0 = crc_u64_parallel3(crc0, chunk_size, &table::SHORT_TABLE, middle_last_first);
 
-    unsafe {
-        let chunk_length = table::SHORT;
-
-        while len >= table::SHORT * 3 {
-            let mut crc1 = 0;
-            let mut crc2 = 0;
-
-            let slice0 = slice::from_raw_parts(ptr, chunk_length);
-            let slice1 =
-                slice::from_raw_parts(ptr.offset((table::SHORT / 8) as isize), chunk_length);
-            let slice2 =
-                slice::from_raw_parts(ptr.offset(((table::SHORT / 8) * 2) as isize), chunk_length);
-
-            for i in 0..chunk_length / 8 {
-                crc0 = self::simd::_mm_crc32_u64(crc0, slice0[i]);
-                crc1 = self::simd::_mm_crc32_u64(crc1, slice1[i]);
-                crc2 = self::simd::_mm_crc32_u64(crc2, slice2[i]);
-            }
-
-            crc0 = table::SHORT_TABLE.shift(crc0) ^ crc1;
-            crc0 = table::SHORT_TABLE.shift(crc0) ^ crc2;
-
-            len -= table::SHORT * 3;
-            ptr = ptr.offset(((table::SHORT / 8) * 3) as isize);
-        }
-    }
-
-    unsafe {
-        let slice = slice::from_raw_parts(ptr, len);
-
-        crc0 = crc_u64(crc0, slice);
-    }
+    crc0 = crc_u64(crc0, middle_last_last);
 
     crc0 = crc_u8(crc0, end);
 
@@ -103,5 +56,39 @@ fn crc_u64(crc: u64, buffer: &[u64]) -> u64 {
         crc, #[target_feature = "+sse4.2"]
         #[target_feature = "+sse4.2"]
         |crc, &next| unsafe { self::simd::_mm_crc32_u64(crc, next) },
+    )
+}
+
+/// Hardware-parallel version of the algorithm.
+fn crc_u64_parallel3(crc: u64, chunk_size: usize, table: &table::CrcTable, buffer: &[u64]) -> u64 {
+    buffer.chunks(chunk_size).fold(
+        crc, #[target_feature = "+sse4.2"]
+        #[target_feature = "+sse4.2"]
+        |mut crc0, chunk| {
+            let mut crc1 = 0;
+            let mut crc2 = 0;
+
+            // Divide it in three
+            let block_size = chunk_size / 3;
+
+            let mut blocks = chunk.chunks(block_size);
+            let a = blocks.next().unwrap();
+            let b = blocks.next().unwrap();
+            let c = blocks.next().unwrap();
+            assert_eq!(blocks.next(), None);
+
+            for i in 0..block_size {
+                unsafe {
+                    crc0 = self::simd::_mm_crc32_u64(crc0, a[i]);
+                    crc1 = self::simd::_mm_crc32_u64(crc1, b[i]);
+                    crc2 = self::simd::_mm_crc32_u64(crc2, c[i]);
+                }
+            }
+
+            crc0 = table.shift(crc0) ^ crc1;
+            crc0 = table.shift(crc0) ^ crc2;
+
+            crc0
+        },
     )
 }
