bc774e238a65d2fb6cf8cc437990738e0793c1af
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-10-09 18:07:50 +0300

    Refactor and document

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
-//! Implements the crc32c algorithm.
+//! This crate provides the CRC-32-Castagnoli algorithm.
+//!
+//! It provides both a software implementation, and a hardware-optimized one for SSE 4.2.
+//!
+//! # Example
+//!
+//! ```rust
+//! let message = b"Hello world!";
+//!
+//! let crc = crc32c::crc32c(message);
+//!
+//! assert_eq!(crc, 0x7B_98_E7_51);
+//! ```
+//!
+//! # Enabling hardware acceleration
+//!
+//! If you compile your code with `-C target-features=+sse4.2`,
+//! then the hardware-optimized version will be compiled into the code.
+//!
+//! Otherwise, the crate will use `cpuid` at runtime to detect the
+//! running CPU's features, and enable the appropiate algorithm.
 
 #![feature(cfg_target_feature, target_feature)]
 
@@ -12,13 +32,15 @@ mod util;
 mod sw;
 mod hw;
 
-/// Computes the crc32c for the data payload.
+/// Computes the CRC for the data payload.
+///
+/// Equivalent to calling `crc32c_append(0, data)`.
 #[inline]
 pub fn crc32c(data: &[u8]) -> u32 {
     crc32c_append(0, data)
 }
 
-/// Computes the crc32c for the data payload, starting with a previous crc32c value.
+/// Computes the CRC for the data payload, starting with a previous CRC value.
 #[inline]
 pub fn crc32c_append(crc: u32, data: &[u8]) -> u32 {
     if cfg_feature_enabled!("sse4.2") {
