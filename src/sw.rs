3719a767d1626a8a895ceb804cd2f5637087b421
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-10-21 11:39:30 +0300

    Refactor `sw` code

--- src/sw.rs
+++ src/sw.rs
@@ -19,39 +19,38 @@ const CRC_TABLE: CrcTable = CrcTable(include!(concat!(env!("OUT_DIR"), "/", "sw.
 
 /// Software implementation of the algorithm.
 pub fn crc32c(crci: u32, buffer: &[u8]) -> u32 {
-    let table = &CRC_TABLE;
-
     let mut crc = u64::from(!crci);
 
     let (start, mid, end) = util::split(buffer);
 
-    crc = crc_u8(table, crc, start);
+    crc = crc_u8(crc, start);
 
-    crc = crc_u64(table, crc, mid);
+    crc = crc_u64(crc, mid);
 
-    crc = crc_u8(table, crc, end);
+    crc = crc_u8(crc, end);
 
     !(crc as u32)
 }
 
 #[inline]
-fn crc_u8(table: &CrcTable, crc: u64, buffer: &[u8]) -> u64 {
+fn crc_u8(crc: u64, buffer: &[u8]) -> u64 {
     buffer.iter().fold(crc, |crc, &next| {
         let index = (crc ^ u64::from(next)) as u8;
-        table.at(0, index) ^ (crc >> 8)
+        CRC_TABLE.at(0, index) ^ (crc >> 8)
     })
 }
 
 #[inline]
-fn crc_u64(table: &CrcTable, crci: u64, buffer: &[u64]) -> u64 {
+fn crc_u64(crci: u64, buffer: &[u64]) -> u64 {
     buffer.iter().fold(crci, |crc, &next| {
         let crc = crc ^ next;
 
         // Note: I've tried refactoring this to a for-loop,
         // but then it gets worse performance.
-        table.at(7, crc as u8) ^ table.at(6, (crc >> 8) as u8) ^ table.at(5, (crc >> 16) as u8) ^
-            table.at(4, (crc >> 24) as u8) ^
-            table.at(3, (crc >> 32) as u8) ^ table.at(2, (crc >> 40) as u8) ^
-            table.at(1, (crc >> 48) as u8) ^ table.at(0, (crc >> 56) as u8)
+        CRC_TABLE.at(7, crc as u8) ^ CRC_TABLE.at(6, (crc >> 8) as u8) ^
+            CRC_TABLE.at(5, (crc >> 16) as u8) ^ CRC_TABLE.at(4, (crc >> 24) as u8) ^
+            CRC_TABLE.at(3, (crc >> 32) as u8) ^
+            CRC_TABLE.at(2, (crc >> 40) as u8) ^ CRC_TABLE.at(1, (crc >> 48) as u8) ^
+            CRC_TABLE.at(0, (crc >> 56) as u8)
     })
 }
