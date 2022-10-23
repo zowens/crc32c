319a315d337bc19a4055bf3610e1f95e204a2aae
Nissa <and-nissa@protonmail.com>
2022-10-22 17:10:25 -0700

    doc(util): document `U64Le` and its single method
    Signed-off-by: Nissa <and-nissa@protonmail.com>

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,14 @@
 use std::{cmp, slice};
 
+/// A newtype wrapper for a little endian `u64`.
 #[repr(transparent)]
 #[derive(Clone, Copy)]
 pub(crate) struct U64Le(u64);
 
 impl U64Le {
+    /// Returns a `u64` with correct endianness for the target.
+    ///
+    /// On little endian targets, this is a no-op.
     #[allow(clippy::inline_always)]
     #[inline(always)]
     pub const fn get(self) -> u64 {
